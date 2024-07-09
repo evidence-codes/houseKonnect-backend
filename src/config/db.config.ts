41aa443147fbcc95887df4d8ea5526c22735b545
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-07-09 18:07:15 +0100

    feat: add database config

---
+++ src/config/db.config.ts
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize-typescript");
+
+const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  models: [__dirname + "/models"],
+});
+
+module.exports = sequelize;
